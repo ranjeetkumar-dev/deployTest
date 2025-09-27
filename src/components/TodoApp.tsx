042b39cb155d1e7e766d327fb65cd74e721095b9
Ranjeet Kumar <ranjeetkumar.dev2@gmail.com>
2025-09-27 23:13:32 +0530

    change

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -152,7 +152,7 @@ const TodoApp = () => {
                 : "from-purple-600 via-pink-600 to-indigo-600"
             } bg-clip-text text-transparent`}
           >
-            0_0 ~_- Todo App 0_0 ~_- 0_0
+            Todo App
           </h1>
           <p
             className={`text-lg ${
