4c07a5c5f0a1d1e61ae787389f46f1a797b88a73
Ranjeet Kumar <ranjeetkumar.dev2@gmail.com>
2025-09-29 18:57:49 +0530

    fix: update TodoApp title for clarity

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -152,7 +152,7 @@ const TodoApp = () => {
                 : "from-purple-600 via-pink-600 to-indigo-600"
             } bg-clip-text text-transparent`}
           >
-            Todo APP APP APPP APPP
+            Todo APP
           </h1>
           <p
             className={`text-lg ${
